5025fbe7126b4a2ddac958add3437cf56f28adab
Joshua Lam <joshualam071522@gmail.com>
2023-09-07 14:40:46 -0700

    added me queryu to clientside

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,24 @@
 const { gql } = require('@apollo/client');
 
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
 export const QUERY_USER = gql`
     query user($username: String, $userId: ID) {
         user(username: $username, userId: $userId) {
